fa241cdd6f684882b3103273725a3e4efc503fb9
Birajit Debbarma <birajitdebbarma@gmail.com>
2023-06-22 09:44:55 +0530

    new panchang

--- server/controller/ApiController.js
+++ server/controller/ApiController.js
@@ -59,42 +59,34 @@ console.log(generateString(10));
 
         exports.pachangPost = async(req, res) =>{
             try{
-                const {lucky_numbers,lucky_colour,presiding_deity,auspicious_dates,panchang_date,panchang_day,vikranm_samvat,shak_samvat,ion,season,month,side,p_date,nakshatra,yoga,rahukal,sunrise,sunset,directional,extra_1,extra_2,extra_3,extra_4,extra_5,extra_6,extra_7,extra_8,panchang_thumbnail,pandeet,} = req.body;
+                const pData = req.body;
                 let uploadPanchang = new PanchangModel({
-                    lucky_numbers: lucky_numbers, 
-                    lucky_colour:lucky_colour,
-                    presiding_deity:presiding_deity,
-                    auspicious_dates:auspicious_dates,
-                    panchang_date:panchang_date,                                    
-                    panchang_day:panchang_day,                                      
-                    vikranm_samvat:vikranm_samvat,
-                    shak_samvat:shak_samvat,                                      
-                    ion:ion,                                     
-                    season:season,                                       
-                    month:month,                                     
-                    side:side,                                     
-                    p_date:p_date,                               
-                    nakshatra:nakshatra,                                
-                    yoga:yoga,                                  
-                    rahukal:rahukal,                                 
-                    sunrise:sunrise,                             
-                    sunset:sunset,                                 
-                    directional:directional,                        
-                    extra_1:extra_1,                                   
-                    extra_2: extra_2,                                   
-                    extra_3:extra_3,                                
-                    extra_4: extra_4,                                     
-                    extra_5:extra_5,                                  
-                    extra_6:extra_6,                                        
-                    extra_7:extra_7,                             
-                    extra_8:extra_8,                                              
-                    panchang_thumbnail:panchang_thumbnail,
-                    pandeet_name:pandeet,                                   
-                    update_date:newDate                      
+                    panchang_thumbnail: pData.panchang_thumbnail,
+                    panchang_title: pData.panchang_title,
+                    panchang_time: pData.panchang_time,
+                    festival_description: pData.festival_description,
+                    sunrise: pData.sunrise,
+                    sunset: pData.sunset,
+                    moonrise: pData.moonrise,
+                    moonset: pData.moonset,
+                    panchang_date: pData.panchang_date,
+                    nakshatra: pData.nakshatra,
+                    yoga: pData.yoga,
+                    karana: pData.karana,
+                    month_amanta: pData.month_amanta,
+                    month_purnimanta: pData.month_purnimanta,
+                    vikram_samvat: pData.vikram_samvat,
+                    shaka_samvat: pData.shaka_samvat,
+                    sun_sign: pData.sun_sign,
+                    moon_sign: pData.moon_sign,
+                    sishashool: pData.sishashool,
+                    moon_placement: pData.moon_placement,
+                    season: pData.season,
+                    ayana: pData.ayana,
+                    publish_date: pData.publish_date,
+                    update_date: newDate                      
                 });
                 await uploadPanchang.save();
-
-
                 res.json(uploadPanchang);
             }catch(error){
                 res.status(400).json({message: error.message})
